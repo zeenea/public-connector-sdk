881d42c6f46f4657d3437c5d07a6395ac45a257e
Arthur Templé <atemple@zeenea.com>
2021-01-18 10:32:13 +0100

    [PLT-853] Add method getConnectorId to ItemConnectorFactory

--- src/main/java/zeenea/sdk/ItemConnectorFactory.java
+++ src/main/java/zeenea/sdk/ItemConnectorFactory.java
@@ -4,6 +4,8 @@ import org.pf4j.ExtensionPoint;
 
 public interface ItemConnectorFactory<T extends SourceItem> extends ExtensionPoint {
 
+    String getConnectorId();
+
     ItemConnector<T> newConnector();
 
 }
