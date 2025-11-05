49c7ac70e49ab2dee3a4631f22c8b94fd92c81f7
Guillaume YAN <guillaume.yan@hcl-software.com>
2025-11-05 17:12:06 +0100

    CommonProperties fixed values

--- src/test/java/zeenea/connector/property/PropertiesBuilderTest.java
+++ src/test/java/zeenea/connector/property/PropertiesBuilderTest.java
@@ -8,9 +8,17 @@ import org.junit.jupiter.api.Test;
 public class PropertiesBuilderTest {
 
   @Test
-  public void testPropertiesBuilder() {
+  public void testPropertiesBuilderByClass() {
     Map<String, PropertyValue> zeenea =
         PropertiesBuilder.create().put(CatalogProperty.of("ZEENEA")).build();
+    var expected = Map.of("Catalog", PropertyValue.tag(List.of("ZEENEA")));
+    Assertions.assertThat(zeenea).containsExactlyInAnyOrderEntriesOf(expected);
+  }
+
+  @Test
+  public void testPropertiesBuilderByStatic() {
+    Map<String, PropertyValue> zeenea =
+        PropertiesBuilder.create().put(CommonPropertyDefinitions.CATALOG, "ZEENEA").build();
     var expected = Map.of("Catalog", PropertyValue.tag(List.of("ZEENEA")));
     Assertions.assertThat(zeenea).containsExactlyInAnyOrderEntriesOf(expected);
   }
