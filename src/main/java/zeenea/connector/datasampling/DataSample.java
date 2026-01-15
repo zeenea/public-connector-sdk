6e90467e3156abd6646d79e295aa3b4738bd8e1d
Baptiste Gousset <baptiste.gousset@hcl-software.com>
2026-01-15 16:57:06 +0100

    Add builder on DataSample to DevX

--- src/main/java/zeenea/connector/datasampling/DataSample.java
+++ src/main/java/zeenea/connector/datasampling/DataSample.java
@@ -1,28 +1,25 @@
 package zeenea.connector.datasampling;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.jetbrains.annotations.NotNull;
+import zeenea.connector.common.IdentificationProperty;
 import zeenea.connector.common.ItemIdentifier;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class DataSample {
 
   private static final ObjectMapper MAPPER = new ObjectMapper();
 
-  @NotNull
-  @SafeVarargs
-  public static DataSample of(List<ItemIdentifier> identifiers, List<SampleValue>... samples) {
-    DataSample dataSample = new DataSample();
-    dataSample.fieldIdentifiers = identifiers;
-    dataSample.samples = samples != null ? Arrays.asList(samples) : List.of();
-    if (!dataSample.isValid()) throw new IllegalArgumentException("Invalid data sampling");
-    return dataSample;
+  private DataSample( List<ItemIdentifier> fieldIdentifiers, List<List<SampleValue>> samples) {
+    this.fieldIdentifiers = fieldIdentifiers;
+    this.samples = samples;
   }
 
   @JsonProperty("fields")
@@ -32,16 +29,67 @@ public class DataSample {
   @JsonProperty("samples")
   List<List<SampleValue>> samples;
 
-  @JsonIgnore
-  public boolean isValid() {
-    return fieldIdentifiers != null
-        && !fieldIdentifiers.isEmpty()
-        && samples != null
-        && !samples.isEmpty()
-        && samples.stream().allMatch(sample -> sample.size() == fieldIdentifiers.size());
+  public static Builder builder(ItemIdentifier... fieldIdentifiers) {
+    return builder(Arrays.asList(fieldIdentifiers));
   }
+
+  public static Builder builder(List<ItemIdentifier> fieldIdentifiers) {
+    return new Builder(fieldIdentifiers);
+  }
+
 
   public String jsonify() throws JsonProcessingException {
     return MAPPER.writeValueAsString(this);
+  }
+
+  public static class Builder {
+
+    private final List<ItemIdentifier> fieldIdentifiers;
+
+    private final List<List<SampleValue>> samples;
+
+    public Builder(List<ItemIdentifier> fieldIdentifiers) {
+
+      if (fieldIdentifiers == null) {
+        throw new IllegalArgumentException("Invalid data sampling: Fields should not be null");
+      }
+      if (fieldIdentifiers.isEmpty()) {
+        throw new IllegalArgumentException("Invalid data sampling: Fields should not be empty");
+      }
+
+      this.fieldIdentifiers = fieldIdentifiers;
+      this.samples = new ArrayList<>();
+    }
+
+    public Builder addRow(SampleValue... row) {
+      return addRow(Arrays.asList(row));
+    }
+
+    public Builder addRow(List<SampleValue> row) {
+
+      if (row != null && fieldIdentifiers.size() != row.size()) {
+        throw new IllegalArgumentException("Invalid data sampling: Inserted Row should have the same size as Fields");
+      }
+
+      this.samples.add(row);
+
+      return this;
+    }
+
+    public DataSample build() {
+      if (samples.isEmpty()) {
+        throw new IllegalArgumentException("Invalid data sampling: Samples should not be empty");
+      }
+
+      return new DataSample(
+              this.fieldIdentifiers,
+              this.samples
+      );
+    }
+
+    public boolean isValid() {
+      return samples.stream().allMatch(sample -> sample.size() == fieldIdentifiers.size());
+    }
+
   }
 }
--- src/test/java/zeenea/connector/datasampling/DataSampleTest.java
+++ src/test/java/zeenea/connector/datasampling/DataSampleTest.java
@@ -3,87 +3,87 @@ package zeenea.connector.datasampling;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+
 import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import zeenea.connector.common.IdentificationProperty;
 import zeenea.connector.common.ItemIdentifier;
 
 class DataSampleTest {
 
-  private static final ItemIdentifier NAME_IDENTIFIER =
-      ItemIdentifier.of(
-          List.of(
-              IdentificationProperty.of("database", "zeenea_db"),
-              IdentificationProperty.of("schema", "music"),
-              IdentificationProperty.of("table", "artists"),
-              IdentificationProperty.of("field", "name")));
+    private static final ItemIdentifier NAME_IDENTIFIER =
+            ItemIdentifier.of(
+                    List.of(
+                            IdentificationProperty.of("database", "zeenea_db"),
+                            IdentificationProperty.of("schema", "music"),
+                            IdentificationProperty.of("table", "artists"),
+                            IdentificationProperty.of("field", "name")));
 
-  private static final ItemIdentifier AGE_IDENTIFIER =
-      ItemIdentifier.of(
-          List.of(
-              IdentificationProperty.of("database", "zeenea_db"),
-              IdentificationProperty.of("schema", "music"),
-              IdentificationProperty.of("table", "artists"),
-              IdentificationProperty.of("field", "age")));
+    private static final ItemIdentifier AGE_IDENTIFIER =
+            ItemIdentifier.of(
+                    List.of(
+                            IdentificationProperty.of("database", "zeenea_db"),
+                            IdentificationProperty.of("schema", "music"),
+                            IdentificationProperty.of("table", "artists"),
+                            IdentificationProperty.of("field", "age")));
 
-  private static final ItemIdentifier ACTIVE_IDENTIFIER =
-      ItemIdentifier.of(
-          List.of(
-              IdentificationProperty.of("database", "zeenea_db"),
-              IdentificationProperty.of("schema", "music"),
-              IdentificationProperty.of("table", "artists"),
-              IdentificationProperty.of("field", "active")));
+    private static final ItemIdentifier ACTIVE_IDENTIFIER =
+            ItemIdentifier.of(
+                    List.of(
+                            IdentificationProperty.of("database", "zeenea_db"),
+                            IdentificationProperty.of("schema", "music"),
+                            IdentificationProperty.of("table", "artists"),
+                            IdentificationProperty.of("field", "active")));
 
-  @Test
-  void jsonify() throws JsonProcessingException {
-    DataSample dataSample =
-        DataSample.of(
-            List.of(NAME_IDENTIFIER, AGE_IDENTIFIER, ACTIVE_IDENTIFIER),
-            List.of(SampleValue.of("Alice"), SampleValue.of(30L), SampleValue.of(false)),
-            List.of(SampleValue.of("Kalle"), SampleValue.of(92L), SampleValue.of(true)));
+    @Test
+    void jsonify() throws JsonProcessingException {
+        DataSample dataSample =
+                DataSample.builder(NAME_IDENTIFIER, AGE_IDENTIFIER, ACTIVE_IDENTIFIER)
+                        .addRow(SampleValue.of("Alice"), SampleValue.of(30L), SampleValue.of(false))
+                        .addRow(SampleValue.of("Kalle"), SampleValue.of(92L), SampleValue.of(true))
+                        .build();
 
-    String expectedJson =
-        "{\"fields\":[{\"database\":\"zeenea_db\",\"schema\":\"music\",\"table\":\"artists\",\"field\":\"name\"},{\"database\":\"zeenea_db\",\"schema\":\"music\",\"table\":\"artists\",\"field\":\"age\"},{\"database\":\"zeenea_db\",\"schema\":\"music\",\"table\":\"artists\",\"field\":\"active\"}],\"samples\":[[\"Alice\",30,false],[\"Kalle\",92,true]]}";
-    assertEquals(expectedJson, dataSample.jsonify());
-  }
+        String expectedJson =
+                "{\"fields\":[{\"database\":\"zeenea_db\",\"schema\":\"music\",\"table\":\"artists\",\"field\":\"name\"},{\"database\":\"zeenea_db\",\"schema\":\"music\",\"table\":\"artists\",\"field\":\"age\"},{\"database\":\"zeenea_db\",\"schema\":\"music\",\"table\":\"artists\",\"field\":\"active\"}],\"samples\":[[\"Alice\",30,false],[\"Kalle\",92,true]]}";
+        assertEquals(expectedJson, dataSample.jsonify());
+    }
 
-  @Test
-  void invalidDataSampling_EmptySamples() {
-    assertThrows(
-        IllegalArgumentException.class, () -> DataSample.of(List.of(NAME_IDENTIFIER), List.of()));
-  }
+    @Test
+    void invalidDataSampling_EmptySamples() {
+        assertThrows(
+                IllegalArgumentException.class, () -> DataSample.builder(NAME_IDENTIFIER).build()
+        );
+    }
 
-  @Test
-  void invalidDataSampling_SampleTooShort() {
-    assertThrows(
-        IllegalArgumentException.class,
-        () ->
-            DataSample.of(
-                List.of(NAME_IDENTIFIER, AGE_IDENTIFIER),
-                List.of(SampleValue.of("Alice")) // Missing age value
-                ));
-  }
+    @Test
+    void invalidDataSampling_SampleTooShort() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () ->
+                        DataSample.builder(NAME_IDENTIFIER, AGE_IDENTIFIER)
+                                .addRow(SampleValue.of("Alice")) // Missing age value;
+        );
+    }
 
-  @Test
-  void invalidDataSampling_SampleTooLong() {
-    assertThrows(
-        IllegalArgumentException.class,
-        () ->
-            DataSample.of(
-                List.of(NAME_IDENTIFIER),
-                List.of(SampleValue.of("Alice"), SampleValue.of(30L)) // Extra value
-                ));
-  }
+    @Test
+    void invalidDataSampling_SampleTooLong() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () ->
+                        DataSample.builder(NAME_IDENTIFIER)
+                                .addRow(SampleValue.of("Alice"), SampleValue.of(30L)) // Extra value
+        );
+    }
 
-  @Test
-  void invalidDataSampling_MixedSampleLengths() {
-    assertThrows(
-        IllegalArgumentException.class,
-        () ->
-            DataSample.of(
-                List.of(NAME_IDENTIFIER, AGE_IDENTIFIER),
-                List.of(SampleValue.of("Alice"), SampleValue.of(30L)), // Valid sample
-                List.of(SampleValue.of("Bob")) // Invalid sample - too short
-                ));
-  }
+    @Test
+    void invalidDataSampling_MixedSampleLengths() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () ->
+                        DataSample.builder(NAME_IDENTIFIER, AGE_IDENTIFIER)
+                                .addRow(SampleValue.of("Alice"), SampleValue.of(30L)) // Valid sample
+                                .addRow(SampleValue.of("Bob")) // Invalid sample - too short
+        );
+    }
 }
