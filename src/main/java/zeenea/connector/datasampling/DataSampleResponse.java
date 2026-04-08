1b20138b2f52918fa4ed32d0d3796277b5fe4c5f
Clément Juste <cjuste@users.noreply.github.com>
2026-04-08 20:27:16 +0200

    [ZEE-10296] Allow an empty data sample response to be returned (#117)

--- src/main/java/zeenea/connector/datasampling/DataSampleResponse.java
+++ src/main/java/zeenea/connector/datasampling/DataSampleResponse.java
@@ -1,16 +1,33 @@
 package zeenea.connector.datasampling;
 
+import java.util.Optional;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 /** Response of a Data Sample */
 public class DataSampleResponse {
 
   /** The sampling result */
-  private final DataSample sample;
+  @Nullable private final DataSample sample;
 
-  public DataSampleResponse(DataSample sample) {
+  /**
+   * Returns an empty DataSampleResponse with no sample. This can be used when sampling fails or is
+   * not applicable, allowing the caller to handle the absence of a sample gracefully.
+   */
+  public DataSampleResponse() {
+    this.sample = null;
+  }
+
+  /**
+   * Returns a DataSampleResponse with the provided sample.
+   *
+   * @param sample that can not be null
+   */
+  public DataSampleResponse(@NotNull DataSample sample) {
     this.sample = sample;
   }
 
-  public DataSample getSample() {
-    return sample;
+  public Optional<DataSample> getSample() {
+    return Optional.ofNullable(sample);
   }
 }
